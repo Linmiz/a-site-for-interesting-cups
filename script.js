09e6837b3d6e59732691748b2b95efa4b6fa3b36
Анжела Гуровська <angelagurovska123@gmail.com>
2025-12-08 19:07:43 +0200

    допрацювання 1 та 2 сторінки, googlesearch

--- script.js
+++ script.js
@@ -53,3 +53,40 @@ document.addEventListener('DOMContentLoaded', () => {
         orderPage.classList.add('hidden');
     }
 });
+
+// пошуковий запит
+const API_KEY = "AIzaSyBWx9Z6CPFDvVxdripsWeGUdeSS9s4w9lk";
+const ENGINE_ID = "27d1b5ba9a30b417f"; 
+
+document.getElementById('meme-search-btn').addEventListener('click', () => {
+    const query = document.getElementById('meme-search').value;
+
+    if (!query.trim()) return;
+
+    fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${ENGINE_ID}&searchType=image&q=${encodeURIComponent(query + " мем")}`)
+        .then(response => response.json())
+        .then(data => {
+            const results = document.getElementById('meme-results');
+            results.innerHTML = "";
+
+            if (!data.items) {
+                results.innerHTML = "<p>Нічого не знайдено (￣ヘ￣)</p>";
+                return;
+            }
+
+            data.items.forEach(item => {
+                const img = document.createElement("img");
+                img.src = item.link;
+                img.alt = "meme";
+                results.appendChild(img);
+                // img.onerror = () => {
+                // card.remove();
+                // };
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            document.getElementById('meme-results').innerHTML =
+                "<p>Сталася помилка при пошуку :(</p>";
+        });
+});
